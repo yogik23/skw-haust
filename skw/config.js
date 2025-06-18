febcea00442d5e9ccace8f413c1b848d067403cd
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-18 22:02:29 +0700

    Update config.js

--- skw/config.js
+++ skw/config.js
@@ -18,8 +18,9 @@ const usdt_address = "0x87054392461F52a513d83EF2e06af50f4e2F6614";
 const wHAUST_address = "0x6C25C1Cb4b8677982791328471be1bFB187687c1";
 
 const abi_swap =["function execute(bytes commands, bytes[] inputs, uint256 deadline) payable"];
+
 const erc20Abi = [
-  "function approve(address spender, uint256 amount) external returns (bool)",
+  "function approve(address guy, uint256 wad) public returns (bool)",
   "function allowance(address owner, address spender) view returns (uint256)"
 ];
 
@@ -57,11 +58,34 @@ function datawHAUSTtoUSDT(amountinput) {
   return wHAUSTtoUSDTInstruction;
 }
 
-async function approve(wallet, tokenAddress, spenderAddress, amount, decimals, expirationTimestamp) {
+async function approve1(wallet, amount) {
+  const token = new ethers.Contract(wHAUST_address, erc20Abi, wallet);
+  const parsedAmount = ethers.parseUnits(amount, 18);
+
+  const allowance = await token.allowance(wallet.address, ca_approve);
+  if (allowance >= parsedAmount) {
+    return;
+  }
+
+  logCache(`Approving tokens...`);
+
+  try {
+    const tx = await token.approve(ca_approve, ethers.MaxUint256);
+    logInfo(`Tx Dikirim https://explorer-testnet.haust.app/tx/${tx.hash}`);
+    await tx.wait();
+    logSuccess(`Approve successful\n`);
+  } catch (err) {
+    logError(`Error during Approve : ${err.message || err}`);
+  }
+}
+
+async function approve2(wallet, tokenAddress, spenderAddress, amount, decimals, expirationTimestamp) {
   const approve_abi = ["function approve(address token, address spender, uint160 amount, uint48 expiration) external"];
   const contract = new ethers.Contract(ca_approve, approve_abi, wallet);
   const parsedAmount = ethers.parseUnits(amount, decimals);
+
   logCache(`Mengirim Approve...`);
+
   try {
     const tx = await contract.approve(tokenAddress, spenderAddress, parsedAmount, expirationTimestamp);
     logInfo(`Tx Dikirim https://explorer-testnet.haust.app/tx/${tx.hash}`);
@@ -92,5 +116,6 @@ export {
   datawHAUSTtoWETH,
   datawHAUSTtoWBTC,
   datawHAUSTtoUSDT,
-  approve,
+  approve1,
+  approve2,
 };
